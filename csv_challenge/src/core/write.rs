fed05cdcf060a07ddff8f3ad43659417c78b0507
Xiuming Lee <lxmcoder@gmail.com>
2022-01-15 21:44:54 +0800

    update

---
+++ csv_challenge/src/core/write.rs
@@ -0,0 +1,6 @@
+use super::*;
+
+pub fn replace_column(data: String, column: &str, replacement: &str) -> Result<String, Error> {
+    let mut lines = data.lines();
+
+}
--- csv_challenge/src/main.rs
+++ csv_challenge/src/main.rs
@@ -1,3 +1,41 @@
+use std::path::PathBuf;
+use std::process;
+use structopt::StructOpt;
+use crate::core::read::{load_csv, write_csv};
+use crate::err::Error;
+use crate::opt::Opt;
+
+mod opt;
+mod err;
+mod core;
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = Opt::from_args();
+    let filename = PathBuf::from(opt.input);
+
+    // 读取内容
+    let csv_data = match load_csv(filename) {
+        Ok(file_context) => {
+            println!("原csv文件内容为：{:?}",file_context);
+            file_context
+        }
+        Err(e) => {
+            println!("main error: {:?}",e);
+            process::exit(1);
+        }
+    };
+
+    // 写入内容
+    let output_file = &opt.output.unwrap_or("output/output.csv".to_string());
+    match write_csv(&csv_data,&output_file) {
+        Ok(_) => {
+            println!("写入文件成功！");
+        }
+        Err(e) => {
+            println!("main error: {:?}",e);
+            process::exit(1);
+        }
+    }
+
+    //println!("{:#?}", opt);
 }
