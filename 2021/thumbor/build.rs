9c774f8a26d0372a63a3c350d73c6cdf62852d1f
Xiuming Lee <lxmcoder@gmail.com>
2022-02-13 15:09:22 +0800

    update...

---
+++ 2021/thumbor/build.rs
@@ -0,0 +1,24 @@
+//use std::process::Command;
+
+fn main() {
+
+    println!("=== 我是build.rs的输出！！ ===");
+
+    //let build_enabled = option_env!("BUILD_PROTO")
+    //    .map(|v| v == "1")
+    //    .unwrap_or(false);
+    //
+    //if !build_enabled {
+    //    println!("=== Skipped compiling protos ===");
+    //    return;
+    //}
+    //
+    prost_build::Config::new()
+        .out_dir("src/pb")
+        .compile_protos(&["abi.proto"], &["."])
+        .unwrap();
+    //Command::new("cargo")
+    //    .args(&["fmt", "--", "src/*.rs"])
+    //    .status()
+    //    .expect("cargo fmt failed");
+}
